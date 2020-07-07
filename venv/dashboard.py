ad67afd2a1bf502ab7e5b2768894c5cf6b68c553
Kunnen <MrK81Jd9v6Y%#z5E>
2020-07-07 13:33:53 -0400

    test

--- venv/dashboard.py
+++ venv/dashboard.py
@@ -84,4 +84,4 @@ def update_bar(position, top_x):
 
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
